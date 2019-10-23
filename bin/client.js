377d8d73af628eaf8eafbb3b041218efa9a7accd
zhangzeshuan <zhangzeshuan@ifanr.com>
2019-10-23 14:05:41 +0800

    Support callback

--- bin/client.js
+++ bin/client.js
@@ -1,11 +1,20 @@
 #!/usr/bin/env node
+const path = require('path')
 const request = require('../lib/client')
 const argv = require('minimist')(process.argv.slice(2))
 
-const configName = argv._[0]
+const configName = argv['c'] || argv['config']
+const callback = argv['callback']
+  ? function (res) {
+    eval(argv['callback'])
+  }
+  : function (res) {
+    console.log(res.data)
+  }
 const env = { ...argv }
 delete env._
 
-request(configName, env)
-  .then(res => console.log(res.data))
+const configFilePathname = path.join(process.env.PWD, configName)
+request(configFilePathname, env)
+  .then(res => callback(res))
   .catch(err => console.log(err))
