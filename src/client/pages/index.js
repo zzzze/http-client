f146dbea970a1334965a52e8e9909001b2a56735
zhangzeshuan <zhangzeshuan@ifanr.com>
2019-12-14 22:03:37 +0800

    Draw nested form

--- src/client/pages/index.js
+++ src/client/pages/index.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import Switch from '@material-ui/core/Switch'
+import Select from '@material-ui/core/Select'
+import MenuItem from '@material-ui/core/MenuItem'
 import Link from 'next/link'
 import axios from 'axios'
 
@@ -7,7 +10,6 @@ const sortTypes = names => {
   const enumType = []
 
   names.forEach(name => {
-    console.log(name)
     if (/^['"].*['"]$/.test(name) ||
       /^\d+$/.test(name) ||
       /^(true|false)$/.test(name)) {
@@ -25,97 +27,122 @@ const sortTypes = names => {
 
 let Type
 
-const renderInput = (property) => {
-  let names = property.type.names
-  names = sortTypes(names)
-  return names.map(name => {
+const Input = ({property}) => {
+  let names = []
+  const [explodeProp, setExplodeProp] = React.useState(false)
+  const [selectedName, setSelectedName] = React.useState(0)
+  if (property.type && property.type.names) {
+    names = sortTypes(property.type.names)
+  }
+  console.log('names', names)
+  if (!names[selectedName]) return null
+  const name = names[selectedName]
+  const renderContent = () => {
     if (Array.isArray(name)) {
       return (
-        <select>
-          {name.map(item => {
-            // item = item.replace(/^['"]|['"]$/g, '')
-            return <option value={item}>{item}</option>
-          })}
-        </select>
+        <div>
+          {property.name} :
+          <select>
+            {name.map(item => {
+              // item = item.replace(/^['"]|['"]$/g, '')
+              return <option value={item}>{item}</option>
+            })}
+          </select>
+        </div>
       )
     }
     if (name === 'string' || name === 'number') {
-      return <input />
+      return (
+        <div>
+          {property.name} :
+          <input value={property.name}/>
+        </div>
+      )
     }
     if (name === 'boolean') {
-      return <select>
-        <option value={true}>true</option>
-        <option value={false}>false</option>
-      </select>
+      return <div>
+        {property.name} :
+        <select>
+          <option value={true}>true</option>
+          <option value={false}>false</option>
+        </select>
+      </div>
+    }
+    if (!!name.properties) {
+      return (
+        <>
+          <div>
+            {property.name} :
+            <Switch
+              checked={explodeProp}
+              onChange={() => {console.log(explodeProp); setExplodeProp(!explodeProp)}}
+              inputProps={{ 'aria-label': 'secondary checkbox' }}
+            />
+          </div>
+          <Type data={name} withoutTitle explodeProp={explodeProp}></Type>
+        </>
+      )
     }
     if (name === 'object') {
-      return <Type data={property}></Type>
+      return <div>
+        {property.name} :
+        <textarea id="" name="" cols="30" rows="10">{name}</textarea>
+      </div>
     }
-    if (name.properties) {
-      return <Type data={name}></Type>
-    }
-  })
-}
-
-const Checkbox = ({value, children, onChange}) => {
-  return (
-    <div>
-      {React.Children.map(children, child => {
-        return React.cloneElement(child, {
-          checked: child.props.value === value,
-          onChange,
-        })
+  }
+  return <div>
+    {names.length > 1 && <Select
+      value={selectedName}
+      onChange={event => setSelectedName(event.target.value)}
+    >
+      {names.map((name, index) => {
+        console.log('name zxx', name, Array.isArray(name))
+        return <MenuItem value={index}>{Array.isArray(name) ? 'Enum' : name}</MenuItem>
       })}
-    </div>
-  )
+    </Select>}
+    
+    {renderContent()}
+  </div>
 }
 
-const CheckboxOption = ({value, checked, onChange}) =>  {
-  const handleChange = React.useCallback(() => {
-    onChange(value)
-  })
-  return (
-    <>
-      <div className={checked ? 'option checked' : 'option'} onClick={handleChange}></div>
-      <style jsx>{`
-        .option {
-          width: 20px;
-          height: 20px;
-          border: 1px solid #333;
-          background: #fff;
-        }
-        .option.checked {
-          border-color: blue;
-          background: blue;
-        }
-      `}</style>
-    </>
-  )
-}
-
-Type = ({data}) => {
-  const [explodeProp, setExplodeProp] = React.useState(false)
+Type = ({data, withoutTitle, explodeProp: explodePropFromParent}) => {
+  const [explodeProp, setExplodeProp] = React.useState(explodePropFromParent)
   React.useEffect(() => {
     if (data.properties) {
       setExplodeProp(true)
     }
   }, [])
-  return (
-    <div>
-      {/* {data.properties} */}
-      {!!data.properties && (
-        <Checkbox value={explodeProp} onChange={(value) => {console.log('value', value); setExplodeProp(value)}}>
-          <CheckboxOption value={true} ></CheckboxOption>
-          <CheckboxOption value={false} ></CheckboxOption>
-        </Checkbox>
-      )}
-      { explodeProp ? data.properties.map(property => {
-        return (
-          <div>{property.name}: {renderInput(property)}</div>
-        )
-      }): <textarea id="" name="" cols="30" rows="10"></textarea>}
-    </div>
-  )
+  React.useEffect(() => {
+    setExplodeProp(explodePropFromParent)
+  }, [explodePropFromParent])
+  if (!!data.properties) {
+    return (
+      <>
+      {!withoutTitle && <div>
+          aaa {data.name}
+          <Switch
+            checked={explodeProp}
+            onChange={() => {console.log(explodeProp); setExplodeProp(!explodeProp)}}
+            inputProps={{ 'aria-label': 'secondary checkbox' }}
+          />
+        </div>}
+        <div>
+          {!!explodeProp ? (
+            data.properties.map(property => {
+              console.log('aaa', property.name)
+              return (
+                <Type data={property} />
+              )
+            })
+          ) : (
+            <textarea id="" name="" cols="30" rows="10">{data.name}</textarea>
+          )}
+        </div>
+      </>
+    )
+  } else {
+    return <><Input property={data} /></>
+  }
 }
 
 const Index = (props) => (
@@ -125,7 +152,7 @@ const Index = (props) => (
     </Link>
     <p>Hello Next.js</p>
     <div>{props.data.description}</div>
-    <Type data={props.data}></Type>
+    <Type data={props.data} explodeProp></Type>
     <pre>{JSON.stringify(props.data, null, '   ')}</pre>
   </div>
 )
